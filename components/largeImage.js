9750ead1c1bc31e0286af5269842c8ce531829d9
Hendrik <mail@hendrikwallbaum.de>
2020-02-19 16:06:58 +0100

    :lipstick: option for noShadow large images.

--- components/largeImage.js
+++ components/largeImage.js
@@ -1,15 +1,18 @@
 /** @jsx jsx */
 import { css, jsx } from '@emotion/core'
 
-const LargeImage = ({ src }) => (
+const LargeImage = ({ src, noShadow = false }) => (
   <img
     css={css`
       margin: 0 auto;
       display: block;
       max-width: 90%;
       max-height: 80%;
-      box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25),
-        0 10px 10px rgba(0, 0, 0, 0.22);
+      ${!noShadow &&
+        css`
+          box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25),
+            0 10px 10px rgba(0, 0, 0, 0.22);
+        `}
     `}
     src={src}
   />
